1823945e5a21165c343a591bce47fbc49d136390
puren802 <puren802@gmail.com>
2018-07-04 17:12:04 +0900

    abc

--- src/test_7_4/BlockchainDriver.java
+++ src/test_7_4/BlockchainDriver.java
@@ -10,10 +10,18 @@ public class BlockchainDriver {
 		//GenesisBlock
 		String[] transactions = {"Hosang sent 1k Bitcoins to Zuckerberg."};
 		Block genesisBlock = new Block(new BlockHeader(null, transactions), transactions);
-		System.out.println(genesisBlock.getBlockHash());
-		//Transaction Forgery
-		transactions[0] = "Hosang sent 10k Bitcoins to Zuckerberg.";
-		genesisBlock = new Block(new BlockHeader(null, transactions), transactions);
-		System.out.println("Block Hash : " + genesisBlock.getBlockHash());
+		System.out.println("Genesis Block Hash :" + genesisBlock.getBlockHash());
+		//Second Block
+		String[] secondTransactions = {"Zuckerberg sent 500 Bitcoins to Hosang."};
+		Block secondBlock = new Block(new BlockHeader(genesisBlock.getBlockHash().getBytes(), secondTransactions), secondTransactions);
+	    System.out.println("Second Block Hash : " + secondBlock.getBlockHash());
+		//Third Block
+	    String[] thirdTransactions = {"Hosang sent 500 Bitcoins to Moon."}; 
+	    Block thirdBlock = new Block(new BlockHeader(secondBlock.getBlockHash().getBytes(), thirdTransactions), thirdTransactions);
+		System.out.println("Third Block Hash : " + thirdBlock.getBlockHash());
+	    //Transaction Forgery
+		//transactions[0] = "Hosang sent 10k Bitcoins to Zuckerberg.";
+		//genesisBlock = new Block(new BlockHeader(null, transactions), transactions);
+		//System.out.println("Block Hash : " + genesisBlock.getBlockHash());
 	}
 }
